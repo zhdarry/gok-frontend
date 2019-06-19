125a04990e2bf915b0ecbdc43f18aa1e162bf6a0
zhdarry <294249270@qq.com>
2019-06-19 18:12:26 +0800

    '增加成员目录'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -23,7 +23,7 @@ module.exports = {
           },
           {
             text: '刘尧',
-            link: '/zhdarry/'
+            link: '/liuyao/'
           },
           {
             text: '秦林',
@@ -60,6 +60,10 @@ module.exports = {
           ['/zhdarry/one', '7 个实用的 vue 开发 tips'], // 子节点，不设置的话，将会从MD文件中读出标题
           ['/zhdarry/two', 'try...catch封装我们的错误处理']
         ]
+      }],
+      '/liuyao/': [{
+        title: '高大帅气的刘大哥', // 显示的节点名称
+        children: []
       }],
       '/css/': [{
         title: 'css',
